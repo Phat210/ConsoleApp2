eaf54e96bf24d12c952b242485b18283a9f02d4f
hoangthanh168 <76901155+hoangthanh168@users.noreply.github.com>
2023-03-29 21:37:45 +0700

    Create object-demo.cs

---
+++ object-demo.cs
@@ -0,0 +1,40 @@
+
+ThueBao thueBao1 = new ThueBao();
+thueBao1.address = "123-Đà Nẵng";
+thueBao1.gender = "Nam";
+thueBao1.fullName = "Trần Văn A";
+thueBao1.dateOfBirth = new DateTime(1990, 1, 1);
+thueBao1.phoneNumber = "0901234567";
+thueBao1.identityNumber = "123456789";
+
+ThueBao thueBao2 = new ThueBao();
+thueBao2.address = "123-Quảng Nam";
+thueBao2.gender = "Nữ";
+thueBao2.fullName = "Nguyễn Thị B";
+thueBao2.dateOfBirth = new DateTime(1995, 2, 2);
+thueBao2.phoneNumber = "0912345678";
+thueBao2.identityNumber = "456789123";
+
+ThueBao thueBao3 = new ThueBao();
+thueBao3.address = "123-Hồ Chí Minh";
+thueBao3.gender = "Nam";
+thueBao3.fullName = "Lê Văn C";
+thueBao3.dateOfBirth = new DateTime(1985, 3, 3);
+thueBao3.phoneNumber = "0923456789";
+thueBao3.identityNumber = "789123456";
+
+ThueBao thueBao4 = new ThueBao();
+thueBao4.address = "123-Hà Nội";
+thueBao4.gender = "Nữ";
+thueBao4.fullName = "Phạm Thị D";
+thueBao4.dateOfBirth = new DateTime(1980, 4, 4);
+thueBao4.phoneNumber = "0934567890";
+thueBao4.identityNumber = "159263478";
+
+ThueBao thueBao5 = new ThueBao();
+thueBao5.address = "123-Đà Lạt";
+thueBao5.gender = "Nam";
+thueBao5.fullName = "Nguyễn Văn E";
+thueBao5.dateOfBirth = new DateTime(1975, 5, 5);
+thueBao5.phoneNumber = "0945678901";
+thueBao5.identityNumber = "753951468";
