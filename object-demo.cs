ca85a0356faf6d9bfe603c170584819e39068826
hoangthanh168 <76901155+hoangthanh168@users.noreply.github.com>
2023-03-29 22:42:14 +0700

    Update object-demo.cs

--- object-demo.cs
+++ object-demo.cs
@@ -1,40 +1,7 @@
-
-ThueBao thueBao1 = new ThueBao();
-thueBao1.address = "123-Đà Nẵng";
-thueBao1.gender = "Nam";
-thueBao1.fullName = "Trần Văn A";
-thueBao1.dateOfBirth = new DateTime(1990, 1, 1);
-thueBao1.phoneNumber = "0901234567";
-thueBao1.identityNumber = "123456789";
-
-ThueBao thueBao2 = new ThueBao();
-thueBao2.address = "123-Quảng Nam";
-thueBao2.gender = "Nữ";
-thueBao2.fullName = "Nguyễn Thị B";
-thueBao2.dateOfBirth = new DateTime(1995, 2, 2);
-thueBao2.phoneNumber = "0912345678";
-thueBao2.identityNumber = "456789123";
-
-ThueBao thueBao3 = new ThueBao();
-thueBao3.address = "123-Hồ Chí Minh";
-thueBao3.gender = "Nam";
-thueBao3.fullName = "Lê Văn C";
-thueBao3.dateOfBirth = new DateTime(1985, 3, 3);
-thueBao3.phoneNumber = "0923456789";
-thueBao3.identityNumber = "789123456";
-
-ThueBao thueBao4 = new ThueBao();
-thueBao4.address = "123-Hà Nội";
-thueBao4.gender = "Nữ";
-thueBao4.fullName = "Phạm Thị D";
-thueBao4.dateOfBirth = new DateTime(1980, 4, 4);
-thueBao4.phoneNumber = "0934567890";
-thueBao4.identityNumber = "159263478";
-
-ThueBao thueBao5 = new ThueBao();
-thueBao5.address = "123-Đà Lạt";
-thueBao5.gender = "Nam";
-thueBao5.fullName = "Nguyễn Văn E";
-thueBao5.dateOfBirth = new DateTime(1975, 5, 5);
-thueBao5.phoneNumber = "0945678901";
-thueBao5.identityNumber = "753951468";
+ List<ThueBao> danhSachThueBao = new List<ThueBao>();
+            danhSachThueBao.Add(new ThueBao("123 Nguyen Van A", ThueBao.GioiTinh.Nam, "Nguyen Van A", new DateTime(1990, 1, 1), "0123456789", "123456789"));
+            danhSachThueBao.Add(new ThueBao("456 Nguyen Van B", ThueBao.GioiTinh.Nu, "Nguyen Van B", new DateTime(1995, 2, 2), "0123456789", "123456789"));
+            danhSachThueBao.Add(new ThueBao("789 Nguyen Van C", ThueBao.GioiTinh.Khac, "Nguyen Van C", new DateTime(2000, 3, 3), "0123456789", "123456789"));
+            danhSachThueBao.Add(new ThueBao("101 Nguyen Van D", ThueBao.GioiTinh.Nam, "Nguyen Van D", new DateTime(2005, 4, 4), "0123456789", "123456789"));
+            danhSachThueBao.Add(new ThueBao("112 Nguyen Van E", ThueBao.GioiTinh.Nu, "Nguyen Van E", new DateTime(2010, 5, 5), "0123456789", "123456789"));
+            
