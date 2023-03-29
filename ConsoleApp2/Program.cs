aeff46f84a52f857e1ee8bbb43157da9e4e4cd46
Phat210 <kutenhatrang001@gmail.com>
2023-03-29 21:27:39 +0700

    xoa het noi dung cua cac ham

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -55,66 +55,28 @@ namespace ConsoleApp2
         // Tìm thành phố có nhiều thuê bao nhất
         public string TimThanhPhoNhieuThueBaoNhat()
         {
-            Dictionary<string, int> cityCountDict = new Dictionary<string, int>();
-            foreach (ThueBao thueBao in danhSachThueBao)
-            {
-                if (cityCountDict.ContainsKey(thueBao.diaChi))
-                {
-                    cityCountDict[thueBao.diaChi]++;
-                }
-                else
-                {
-                    cityCountDict.Add(thueBao.diaChi, 1);
-                }
-            }
-            return cityCountDict.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
+          
         }
         public string TimThanhPhoItThueBaoNhat()
         {
-            Dictionary<string, int> cityCountDict = new Dictionary<string, int>();
-            foreach (ThueBao thueBao in danhSachThueBao)
-            {
-                if (cityCountDict.ContainsKey(thueBao.diaChi))
-                {
-                    cityCountDict[thueBao.diaChi]++;
-                }
-                else
-                {
-                    cityCountDict.Add(thueBao.diaChi, 1);
-                }
-            }
-            return cityCountDict.Aggregate((x, y) => x.Value < y.Value ? x : y).Key;
+          
         }
         // Tìm thuê bao sở hữu ít số điện thoại nhất
         public ThueBao TimThueBaoItSDTNhat()
         {
-            return danhSachThueBao.OrderBy(t => t.sDT.Length).FirstOrDefault();
+           
         }
 
         // Sắp xếp khách hàng theo họ tên (tăng dần hoặc giảm dần)
         public ThueBao[] SapXepTheoHoTen(bool tangDan)
         {
-            if (tangDan)
-            {
-                return danhSachThueBao.OrderBy(t => t.hoTen).ToArray();
-            }
-            else
-            {
-                return danhSachThueBao.OrderByDescending(t => t.hoTen).ToArray();
-            }
+          
         }
 
         // Sắp xếp khách hàng theo số lượng số điện thoại sở hữu (tăng dần hoặc giảm dần)
         public ThueBao[] SapXepTheoSoDT(bool tangDan)
         {
-            if (tangDan)
-            {
-                return danhSachThueBao.OrderBy(t => t.sDT.Length).ToArray();
-            }
-            else
-            {
-                return danhSachThueBao.OrderByDescending(t => t.sDT.Length).ToArray();
-            }
+           
         }
     }
 
@@ -123,18 +85,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            List<ThueBao> arr = new List<ThueBao>()
-            {
-                new ThueBao() { diaChi = "Hà Nội", gioiTinh = GioiTinh.Nam, hoTen = "Nguyễn Văn A", ngaySinh = DateTime.Now.AddYears(-30), sDT = "123456789", soCMND = "0123456789" },
-                new ThueBao() { diaChi = "Đà Nẵng", gioiTinh = GioiTinh.Nu, hoTen = "Trần Thị B", ngaySinh = DateTime.Now.AddYears(-25), sDT = "4567", soCMND = "9876543210" },
-                new ThueBao() { diaChi = "Hồ Chí Minh", gioiTinh = GioiTinh.Khac, hoTen = "Lý Tấn C", ngaySinh = DateTime.Now.AddYears(-40), sDT = "987654321", soCMND = "1111111111" }
-            };
-            // phat them 1 dong moi
-            DanhBa danhBa = new DanhBa(arr);
-            string thanhPhoNhieuNhat = danhBa.TimThanhPhoNhieuThueBaoNhat();
-            string thanhPhoItNhat = danhBa.TimThanhPhoItThueBaoNhat();
-            ThueBao thueBaoItSDTNhat = danhBa.TimThueBaoItSDTNhat();
-            ThueBao[] sapXepTheoHoTenTangDan = danhBa.SapXepTheoHoTen(true);
+           
         }
     }
 }
